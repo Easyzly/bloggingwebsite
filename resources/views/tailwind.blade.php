02fa7cdb175b676b67d993747ecbee21f8489f01
Jamie.vg | Easyzly <117265805+Easyzly@users.noreply.github.com>
2024-02-14 12:01:20 +0100

    +styling header & webshop template

--- resources/views/tailwind.blade.php
+++ resources/views/tailwind.blade.php
@@ -21,9 +21,10 @@
     main{ flex:1; }
 </style>
 
-<body class="bg-slate-100 min-h-screen">
+<body class="bg-slate-200 min-h-screen">
+{{--header--}}
     <header class="bg-slate-50 shadow-sm">
-        <div class="flex align-center sm:justify-normal justify-between sm:max-w-3xl m:max-w-4xl ml:max-w-5xl l:max-w-6xl m-auto text-gray-700/80 text-m">
+        <div class="flex align-center sm:justify-normal justify-between sm:max-w-3xl m:max-w-4xl ml:max-w-5xl l:max-w-6xl m-auto text-gray-800/80 text-m">
             <button id="menu-toggle" class="sm:hidden px-2 py-1">
                 <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24"
                      xmlns="http://www.w3.org/2000/svg">
@@ -57,21 +58,38 @@
             <a class="my-auto" href="#home"><img class="h-6 my-auto" src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c5/Target_Corporation_logo_%28vector%29.svg/1200px-Target_Corporation_logo_%28vector%29.svg.png"></a>
 
             <div class="hidden sm:flex">
-                <a href="#home" class="hover:scale-105 duration-100 self-end px-4 py-4 border-b-2 border-slate-400/0 hover:bg-slate-100/60 hover:border-blue-500 hover:text-gray-800/80">Jamie van Gulik</a>
-                <a href="#projects" class="hover:scale-105 duration-100 ml-2 self-end px-4 py-4 border-b-2 border-slate-400/0 hover:bg-slate-100/60 hover:border-blue-500 hover:text-gray-800/80">Projecten</a>
-                <a href="#websites" class="hover:scale-105 duration-100 ml-2 self-end px-4 py-4 border-b-2 border-slate-400/0 hover:bg-slate-100/60 hover:border-blue-500 hover:text-gray-800/80">Websites</a>
-                <a href="#blogs" class="hover:scale-105 duration-100 ml-2 self-end px-4 py-4 border-b-2 border-slate-400/0 hover:bg-slate-100/60 hover:border-blue-500 hover:text-gray-800/80">Blogs</a>
+                <a href="#home" class="hover:scale-105 duration-100 ml-2 self-end px-4 py-4 border-b-2 border-slate-400/0 hover:bg-slate-100 hover:border-blue-500 hover:text-gray-800">Jamie van Gulik</a>
+                <a href="#projects" class="hover:scale-105 duration-100 ml-2 self-end px-4 py-4 border-b-2 border-slate-400/0 hover:bg-slate-100 hover:border-blue-500 hover:text-gray-800">Projecten</a>
+                <a href="#websites" class="hover:scale-105 duration-100 ml-2 self-end px-4 py-4 border-b-2 border-slate-400/0 hover:bg-slate-100 hover:border-blue-500 hover:text-gray-800">Websites</a>
+                <a href="#blogs" class="hover:scale-105 duration-100 ml-2 self-end px-4 py-4 border-b-2 border-slate-400/0 hover:bg-slate-100 hover:border-blue-500 hover:text-gray-800">Blogs</a>
             </div>
 
             <div class="hidden sm:flex grow"></div>
-            <a href="#account" class="hover:scale-[1.3] duration-100 scale-125 mx-2 self-end px-4 py-4 border-b-2 border-slate-400/0 hover:bg-slate-100/60"><ion-icon name="person-outline"></ion-icon></a>
+            <a class="my-auto" href="#account"><img class="h-6 my-auto" src="https://cdn-icons-png.freepik.com/256/1144/1144760.png"></a>
+
         </div>
     </header>
-
-    <main>
-        @yield('content')
+{{--Main--}}
+    <main class="my-16">
+        <div class="flex justify-between m-auto sm:max-w-3xl m:max-w-4xl ml:max-w-5xl l:max-w-6xl text-3xl mb-5 text-gray-800/95">
+            <h1>Webshop</h1>
+            <h1>T-Shirts</h1>
+        </div>
+        <div class="grid grid-cols-3 m-auto gap-10 sm:max-w-3xl m:max-w-4xl ml:max-w-5xl l:max-w-6xl">
+            @for ($x = 0; $x <= 29; $x++)
+                <div>
+                    <div class="aspect-[5/6] bg-green-600 rounded"></div>
+                    <div class="m-auto pt-2 pb-0.5 flex justify-between align-center">
+                        <p class="text-gray-800/95">Sweater</p>
+                        <p class="text-gray-800/95">40,00$</p>
+                    </div>
+                    <p class="text-xs text-gray-800/80">Charcoal</p>
+                </div>
+            @endfor
+        </div>
     </main>
 
+{{--Footer--}}
     <footer class="bg-slate-50 shadow-sm">
         <div class="flex align-center sm:justify-normal justify-between sm:max-w-3xl m:max-w-4xl ml:max-w-5xl l:max-w-6xl m-auto text-gray-700/80 text-sm text-center">
             <a href="#socials" class="px-2 sm:px-4 w-full z-10 py-2 hover:bg-gray-200">Socials</a>
